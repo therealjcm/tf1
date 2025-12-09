43b58a226874f144f33ec64f92100f1341d18cfd
AVMUser-57485543@LODSPRODMCA.onmicrosoft.com <AVMUser-57485543@LODSPRODMCA.onmicrosoft.com>
2025-12-09 15:10:36 -0800

    added import of existing storage account and rg

---
+++ storage.tf
@@ -0,0 +1,31 @@
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_resource_group" "imported_rg" {
+    name = "rg-unmanaged-1"
+    location = "northeurope"
+}
+
+import {
+    to = azurerm_resource_group.imported_rg
+    id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/rg-unmanaged-1"
+}
+
+resource "azurerm_storage_account" "imported_st" {
+  resource_group_name               = "rg-unmanaged-1"
+  access_tier                       = "Hot"
+  account_kind                      = "StorageV2"
+  account_replication_type          = "LRS"
+  account_tier                      = "Standard"
+  location                          = "northeurope"
+  name                              = "stheccjcmlab1"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+import {
+  to = azurerm_storage_account.imported_st
+  id = "/subscriptions/${data.azurerm_client_config.current.subscription_id}/resourceGroups/rg-unmanaged-1/providers/Microsoft.Storage/storageAccounts/stheccjcmlab1"
+}
+
